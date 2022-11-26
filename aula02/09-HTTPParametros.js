92490c0514746d1514146710b1104ef6f28c8fbd
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-11-25 22:29:35 -0300

    Finish aula02

---
+++ aula02/09-HTTPParametros.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const url = require('url');
+
+const servidor = http.createServer((req, res) => {
+  // Captura os parâmetros enviados via GET
+
+  let parametros = url.parse(req.url, true);
+
+  let nome = parametros.query.nome;
+
+  res.setHeader('Content-Type', 'text/html');
+
+  res.end(`
+  
+    <form>
+      <label for="nome">Digite Seu nome</label>
+      <input type="text" name='nome' id='nome'>
+      <input type="submit" value="Enviar">
+    </form>
+
+    <h1>Olá ${nome ? nome : 'Random'}</h1>
+
+  `);
+})
+
+servidor.listen(8888, () => {
+  console.log('Servidor: http://localhost:8888');
+})
