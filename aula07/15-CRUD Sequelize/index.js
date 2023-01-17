49b081fd1da880fd4171c2ab5ac3a834844cd785
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2023-01-16 21:23:17 -0300

    Sequelize routes

---
+++ aula07/15-CRUD Sequelize/index.js
@@ -0,0 +1,23 @@
+// Importa o Módulo do Express (facilitador na criação de servidores)
+const express = require('express');
+
+// Inicializa o Express
+const app = express();
+
+const porta = 5000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/cadastro', (req, res) => {
+  res.sendFile(__dirname + '/views/cadastro.html')
+})
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em: http://localhost:${porta}`);
+})
